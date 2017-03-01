a71fe254417f196b5eefbd6832e262129e432ee7
adam123 <wjw3323@live.com>
2017-03-01 10:20:39 +0800

    优化粉丝数中文转换

--- bili_spider/user_info.py
+++ bili_spider/user_info.py
@@ -5,15 +5,15 @@ user_config = {
     'gender': "//span[@id='h-gender']/@class",
     'level': "//a[@class='h-level m-level']/@lvl",
     'register_time': "//div[@class='item regtime']/span[@class='text']/text()",
-    'location': "//div[@class='item geo']/span[@class='text guest']/text()"
+    'location': "//div[@class='item geo']/span[@class='text']/text()"
 }
 
 extend_config = {
     'head_img': "//div[@class='h-avatar']/img/@src",
     'sign': "//div[@class='h-sign']/text()",
-    'birthday': "//div[@class='item birthday']/span[@class='text guest']/text()",
-    'follow_num': "//a[@class='item data-gz']/span[@class='quantity']/text()",
-    'fans_num': "//a[@class='item data-fs']/span[@class='quantity space-fans']/text()"
+    'birthday': "//div[@class='item birthday']/span[@class='text']/text()",
+    'follow_num': "//div[@class='friend-data']/a[@href='#!/fans/follow']/span[@class='quantity']/text()",
+    'fans_num': "//div[@class='friend-data']/a[@href='#!/fans/fans']/span[@class='quantity space-fans']/text()"
 }
 
 
